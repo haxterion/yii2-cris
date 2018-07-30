9eb6d95eb072a7f284cfc78dc54ffdabc46239f8
Catalist21 <41837444+Catalist21@users.noreply.github.com>
2018-07-30 17:35:01 +0700

    jj

--- frontend/views/schedule/_form.php
+++ frontend/views/schedule/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use yii\helpers\ArrayHelper;
+use frontend\models\Vehicle;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Schedule */
@@ -12,9 +14,16 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'id_booking')->textInput() ?>
+    <!-- <?= $form->field($model, 'id_booking')->textInput() ?> -->
 
-    <?= $form->field($model, 'id_vehicle')->textInput() ?>
+    <!-- <?= $form->field($model, 'id_vehicle')->textInput() ?>  -->
+
+    <?= $form->field($model,'vehicle')->dropDownList(
+        ArrayHelper::map(Vehicle::find()->all(),'id', 'licence_plat'),
+        ['prompt'=>'Choose Licence Plat']
+    ) ?>
+
+    
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
