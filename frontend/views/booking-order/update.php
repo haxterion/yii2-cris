e80861bf761bd16c106d216392ee6037eb8c0b5c
rijalimnida <rijalimnida@gmail.com>
2018-07-30 11:42:07 +0700

    Pembetulan

--- frontend/views/booking-order/update.php
+++ frontend/views/booking-order/update.php
@@ -11,7 +11,6 @@ $this->params['breadcrumbs'][] = ['label' => $model->id, 'url' => ['view', 'id' 
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="booking-order-update">
-<?= $this->render('header');?>
 <br>
 <br>
 <br> 
