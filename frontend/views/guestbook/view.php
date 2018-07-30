ae605750f886807a24c850908a96ff5daac13fad
sofie15 <mariaalfi30@gmail.com>
2018-07-30 12:10:16 +0700

    guestrecord

--- frontend/views/guestbook/view.php
+++ frontend/views/guestbook/view.php
@@ -3,8 +3,12 @@
 use yii\helpers\Html;
 use kartik\detail\DetailView;
 use yii\widgets\ActiveForm;
-use yii\widgets\Pjax;
+/*use yii\widgets\Pjax;*/
 use yii\grid\GridView;
+use frontend\models\User;
+use kartik\date\DatePicker;
+use yii\helpers\ArrayHelper;
+use frontend\models\Guestbook;
 
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Guestbook */
@@ -25,7 +29,7 @@ $this->params['breadcrumbs'][] = $this->title;
         ]) ?>
     </p>
  <div class="row">
-        <div class="col-sm-4">
+        <div class="col-sm-3">
     <?= DetailView::widget([
         'model' => $model,
         'bootstrap' => true,
@@ -46,4 +50,69 @@ $this->params['breadcrumbs'][] = $this->title;
         ],
     ]) ?>
 </div>
+<div class="col-sm-3">
+    <div class="recordguestbook-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+<div class="row">
+        <div class="col-sm-2">
+    <?= $form->field($model, 'id_user')->textInput(['readOnly' => true]) ?>
+        </div>
+        <div class="col-sm-6">
+    <?= $form->field($model, 'person_name')->textInput(['readOnly' => true]) ?>
+        </div>
+        <div class="col-sm-3">
+    <?= $form->field($model, 'date_transaksi')->widget(
+        DatePicker::className(),[
+            'name' => 'check_issue_date',
+            'value' => date('d-M-Y', strtotime('+2 days')),
+            'options' => ['placeholder' => 'Select Issue date....'],
+            'pluginOptions' => [
+                'format' => 'yyyy-mm-dd',
+                'todayHighlight' => true,
+                'autoclose'=>true
+            ]
+        ]);?>
+        </div>
+        <div class="col-sm-2">
+    <?= $form->field($model, 'id_guestbook')->dropDownList(
+        ArrayHelper::map(Guestbook::find()->all(),'id', 'id'),
+        ['prompt'=>'Choose Id Guestbook']
+    ) ?>
+        </div>
+        <div class="col-sm-3">
+    <?= $form->field($model, 'date_phone')->widget(
+        DatePicker::className(),[
+            'name' => 'check_issue_date',
+            'value' => date('d-M-Y', strtotime('+2 days')),
+            'options' => ['placeholder' => 'Select Issue date....'],
+            'pluginOptions' => [
+                'format' => 'yyyy-mm-dd',
+                    'todayHighlight' => true,
+                    'autoclose'=>true
+            ]
+        ]);?>
+        </div>
+        <div class="col-sm-3">
+    <?= $form->field($model, 'price')->textInput() ?>
+        </div>
+        <div class="col-sm-3">
+    <?= $form->field($model, 'status')->radioList(array('intro'=>'Intro','nego'=>'Nego' ,'deal'=>'Deal', 'reject'=>'Reject'))?>
+        </div>
+        <div class="col-sm-11">
+    <?= $form->field($model, 'explanation')->textArea(['maxlength' => true]) ?>
+        </div></div>
+
+
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Lihat Data', ['/guestbook'], ['class' => 'btn btn-default', 'target' => '_blank']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
+     </div>
 </div>
