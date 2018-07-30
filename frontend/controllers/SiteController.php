bd2db63dbdb35e41f8da816dcf28e306f238b4ed
Catalist21 <41837444+Catalist21@users.noreply.github.com>
2018-07-30 09:56:43 +0700

    form schedule

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -237,16 +237,16 @@ class SiteController extends Controller
         ]);
     }
 
-    public function actionSchedule()
-    {
-        $model = new Schedule() {
+    // public function actionSchedule()
+    // {
+    //     //$model = new Schedule() {
 
 
-    }
+    // }
 
-    return $this->render('schedule',[
-        'model' => $model,
-        ]);
-    }
+    // return $this->render('schedule',[
+    //     'model' => $model,
+    //     ]);
+    // }
 
 }
